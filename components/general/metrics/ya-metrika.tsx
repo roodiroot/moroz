d6f5852a036cd41b4e19b0a3b71c93350819a6f2
Maxim <maxim@MacBook-Air-Maxim.local>
2026-03-26 12:21:00 +0300

    add metrics

--- components/general/metrics/ya-metrika.tsx
+++ components/general/metrics/ya-metrika.tsx
@@ -17,18 +17,14 @@ const YAMetrika = () => {
     <>
       <Script id="yandex-metrika">
         {`
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();
         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
 
-        ym(94961323, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
-        });
+    ym(94961323, 'init', {webvisor:true, clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
       `}
       </Script>
       {/* <script
